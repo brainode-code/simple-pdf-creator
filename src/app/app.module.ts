faf34dadba22ffc9023bd63daf07532da1628a10
brainode-code <grzegorz.serwin+brainode@gmail.com>
2020-01-25 21:15:45 +0100

    basic editor init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    CanvasComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
